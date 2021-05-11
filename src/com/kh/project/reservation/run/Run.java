b843d4d9d58d7b3beeded4f2215b4ec0664310a1
shjpoqw <shj0949@naver.com>
2021-05-11 14:43:37 +0900

    김명선

--- src/com/kh/project/reservation/run/Run.java
+++ src/com/kh/project/reservation/run/Run.java
@@ -5,7 +5,10 @@ public class Run {
 	public static void main(String[] args) {
 		
 		System.out.println("default");
-		System.out.println("ms");
+		int i = 0;
+		if (i==0) {
+			System.out.println("ms");
+		}
 	}
 
 }
