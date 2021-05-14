271318e4cb164de230988e888edd635489e30f8f
Furaha-DH <pastelto@naver.com>
2021-05-14 14:24:27 +0900

    2. 수정중

--- src/com/kh/project/reservation/controller/ReservationManager.java
+++ src/com/kh/project/reservation/controller/ReservationManager.java
@@ -5,7 +5,7 @@ public class ReservationManager {
 	public void checkMyReservation() { // 매개변수 : 예약날짜, 예약좌석, 예약성함, 예약상태 등
 		
 		// (1) 현재 예약 상황 --> 없으면 return null; / 있으면 현재 예약 정보 보여주기
-		
+		// (2) 커밋 수정중
 		
 	}
 
