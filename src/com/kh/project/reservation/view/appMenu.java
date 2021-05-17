64b997449e72b395fdc3d1629738421d3249f7d8
dayeo <dayeong815@gmail.com>
2021-05-17 19:09:36 +0900

    콘솔창 돌려보려고 초기화시켰던 것들 삭제하고 다시 올려요

--- src/com/kh/project/reservation/view/appMenu.java
+++ src/com/kh/project/reservation/view/appMenu.java
@@ -1,8 +1,12 @@
 package com.kh.project.reservation.view;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
+import javax.swing.text.html.HTMLDocument.Iterator;
+
 import com.kh.project.reservation.controller.BeverageManager;
+import com.kh.project.reservation.controller.CheckAccount;
 import com.kh.project.reservation.controller.PrintServiceManager;
 import com.kh.project.reservation.controller.ReservationManager;
 import com.kh.project.reservation.controller.TicketManager;
@@ -19,12 +23,9 @@ public class appMenu {
 	TicketManager tm = new TicketManager();
 	Drink d = new Drink();
 	Account a = new Account();
+	
 	public void mainMenu() { // 화면 구현 후 사라질 클래스.. Maybe..
-		{ 
-			a.setPay("1234");
-			a.setCoupon(1);
-		}
-
+	
 		// 로그인 창
 
 		// 회원가입 창
@@ -34,78 +35,78 @@ public class appMenu {
 
 		
 	
-//	public appMenu() {
-//		HashMap<String, Account> membership = new HashMap<String, Account>();
-//		ReservationManager rm = new ReservationManager();
-//		CheckAccount ca = new CheckAccount();
-//	
-//		Scanner sc = new Scanner(System.in);
-//		
-//		//로그인 창
-//		while(true) {
-//
-//			System.out.println("로그인");
-//			String id = sc.nextLine().trim();
-//			
-//			System.out.println("비밀번호");
-//			String pw = sc.nextLine().trim();
-//			
-//			//if(rm.checkIdNPw() 가 맞는지 체크 맞으면 메뉴 페이지, 아니면 팝업창도록 수정
-//			
-//			if(!membership.containsKey(id))
-//			{
-//				System.out.println("입력하신 id는 존재하지 않습니다." + " 다시 입력해주세요.");
-//				continue;
-//			}
-//			else
-//			{
-//				if(!membership.get(id).equals(pw))
-//				{
-//					System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
-//				}
-//				else
-//				{
-//					System.out.println("id와 비밀번호가 일치합니다!");
-//					break;
-//				}
-//				// 회원가입 버튼 클릭시 추가
-//			}
-//		}
-//		//회원가입 창 
-//		System.out.println("로그인");
-//		String id = sc.nextLine();
-//		
-//		System.out.println("비밀번호");
-//		String pw = sc.nextLine();
-//		
-//		System.out.println("이름");
-//		String name = sc.nextLine();
-//		
-//		System.out.println("주민번호");
-//		String idNum = sc.nextLine();
-//		
-//		System.out.println("성별");
-//		char gender = sc.nextLine().charAt(0);
-//		
-//		System.out.println("핸드폰 번호");
-//		String pNum = sc.nextLine();
-//		
-//		System.out.println("은행");
-//		String bank = sc.nextLine();
-//		
-//		System.out.println("카드번호");
-//		String pay = sc.nextLine();
-//		
-//		//if()문으로 일치 확인
-//
-//		if(membership.isEmpty()) {
-//			System.out.println("없습니다.");
-//		}else {
-//			Iterator<String> it = map.keySet().iterator();
-//			while(it.hasNext()) {
-//				System.out.println(map.get(it.next()));
-//			}
-//		}
+	public appMenu() {
+		HashMap<String, Account> membership = new HashMap<String, Account>();
+		ReservationManager rm = new ReservationManager();
+		CheckAccount ca = new CheckAccount();
+	
+		Scanner sc = new Scanner(System.in);
+		
+		//로그인 창
+		while(true) {
+
+			System.out.println("로그인");
+			String id = sc.nextLine().trim();
+			
+			System.out.println("비밀번호");
+			String pw = sc.nextLine().trim();
+			
+			//if(rm.checkIdNPw() 가 맞는지 체크 맞으면 메뉴 페이지, 아니면 팝업창도록 수정
+			
+			if(!membership.containsKey(id))
+			{
+				System.out.println("입력하신 id는 존재하지 않습니다." + " 다시 입력해주세요.");
+				continue;
+			}
+			else
+			{
+				if(!membership.get(id).equals(pw))
+				{
+					System.out.println("비밀번호가 일치하지 않습니다. 다시 입력해주세요.");
+				}
+				else
+				{
+					System.out.println("id와 비밀번호가 일치합니다!");
+					break;
+				}
+				// 회원가입 버튼 클릭시 추가
+			}
+		}
+		//회원가입 창 
+		System.out.println("로그인");
+		String id = sc.nextLine();
+		
+		System.out.println("비밀번호");
+		String pw = sc.nextLine();
+		
+		System.out.println("이름");
+		String name = sc.nextLine();
+		
+		System.out.println("주민번호");
+		String idNum = sc.nextLine();
+		
+		System.out.println("성별");
+		char gender = sc.nextLine().charAt(0);
+		
+		System.out.println("핸드폰 번호");
+		String pNum = sc.nextLine();
+		
+		System.out.println("은행");
+		String bank = sc.nextLine();
+		
+		System.out.println("카드번호");
+		String pay = sc.nextLine();
+		
+		//if()문으로 일치 확인
+
+		if(membership.isEmpty()) {
+			System.out.println("없습니다.");
+		}else {
+			Iterator<String> it = map.keySet().iterator();
+			while(it.hasNext()) {
+				System.out.println(map.get(it.next()));
+			}
+		}
 
 		
 		while(true) { // 나중에 GUI랑 연결 // true값 대신 로그인 조건 받아와야할듯..?
