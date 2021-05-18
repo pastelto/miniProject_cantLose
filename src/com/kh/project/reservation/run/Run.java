b5cebcdeea1719d85d9a75dc8b95bed24877e93a
ahhye <ahhye@DESKTOP-ROOKH21>
2021-05-18 16:42:25 +0900

    의미없수

--- src/com/kh/project/reservation/run/Run.java
+++ src/com/kh/project/reservation/run/Run.java
@@ -1,15 +1,16 @@
 package com.kh.project.reservation.run;
 
 
-import com.kh.project.reservation.view.MenuChoice;
-import com.kh.project.reservation.view.appMenu;
+import com.kh.project.reservation.view.Drink;
 
 public class Run {
 
 	public static void main(String[] args) {
 
 		//MenuChoice m = new MenuChoice();
-		new appMenu().mainMenu();
+		//new appMenu().mainMenu();
+		
+		Drink d = new Drink();
 
 
 
