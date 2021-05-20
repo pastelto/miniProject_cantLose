e731c4a711764c5593dfa6f27f12c541d849fc3f
nakaz228 <jyl228@naver.com>
2021-05-20 14:54:41 +0900

    2021-05-20- 주연01

--- src/com/kh/project/reservation/view/Login.java
+++ src/com/kh/project/reservation/view/Login.java
@@ -1,11 +1,16 @@
 package com.kh.project.reservation.view;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -13,19 +18,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import com.kh.project.reservation.controller.CheckAccount;
 import com.kh.project.reservation.model.vo.Account;
+import com.kh.project.reservation.reservation.view.MenuChoice;
+import com.kh.project.reservation.reservation.view.SignUp;
 
 public class Login extends JFrame {
 
 	Scanner sc = new Scanner(System.in);
 	HashMap<String, Account> account = new HashMap<>();
 	JButton login = new JButton("로그인");
-	JButton signout = new JButton("회원가입");
+	JButton signUp = new JButton("회원가입");
 	JTextField logTF = new JTextField();
 	JPasswordField pwTF = new JPasswordField();
 	JPanel loginP = new JPanel();
@@ -65,17 +73,17 @@ public class Login extends JFrame {
 		login.setLocation(40, 370);
 		login.setSize(270, 54);
 
-		signout.setLocation(40, 440);
-		signout.setSize(270, 54);
+		signUp.setLocation(40, 440);
+		signUp.setSize(270, 54);
 
 		loginP.add(logTF);
 		loginP.add(pwTF);
 		loginP.add(login);
-		loginP.add(signout);
+		loginP.add(signUp);
 		loginP.add(image);
 		this.add(loginP);
 		
-		id = logTF.getText();
+		/*id = logTF.getText();
 		pw = pwTF.getText(); //?????왜 줄쳐지는거지...
 		
 		login.addMouseListener(new MouseAdapter() { // 로그인 클릭시 일치하면 여기
@@ -92,7 +100,7 @@ public class Login extends JFrame {
 					//}
 				}
 			}
-		});
+		}); 
 
 		signout.addMouseListener(new MouseAdapter() { // 회원가입 여기
 
@@ -103,10 +111,48 @@ public class Login extends JFrame {
 						setVisible(false);
 			} 
 			}
+		});  */
+
+		
+		login.addActionListener(new ActionListener() { // 로그인 클릭시 일치하면 여기
+
+			@Override
+			public void actionPerformed(ActionEvent e2) {//로그인 할때 
+				// TODO Auto-generated method stub
+				try{
+					String s;
+					String[] array;
+					BufferedReader bos = new BufferedReader(new FileReader(id+".txt"));
+					while((s=bos.readLine())!=null){
+						array=s.split("/");
+					if(logTF.getText().equals(array[0])&&pwTF.getText().equals(array[1]))
+					{
+						JOptionPane.showMessageDialog(null, "로그인이 되었습니다!!");
+						MenuChoice menu = new MenuChoice();
+					}
+					else 
+					{
+						JOptionPane.showMessageDialog(null, "로그인이 실패하였습니다.");
+					}
+					}
+					bos.close();
+					dispose();
+				}catch (IOException e){
+					JOptionPane.showMessageDialog(null, "회원가입이 필요합니다.");
+				}
+			}
+		});
+
+		signUp.addMouseListener(new MouseAdapter() { // 회원가입 여기
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if (e.getButton() == 1) {
+						new SignUp();
+						setVisible(false);
+			} 
+			}
 		}); 
-
-
-
 
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -114,3 +160,4 @@ public class Login extends JFrame {
 	}
 
 }
+
