b3fb1d234c1982544199500e7ad0e813e104d9f5
wangsubin <wangsubin@localhost>
2021-05-20 15:22:55 +0900

    못생겼지만 일단 3분의 1정도는 연결했습니다.

--- src/com/kh/project/reservation/view/BookView.java
+++ src/com/kh/project/reservation/view/BookView.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
 
 import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
@@ -12,123 +13,178 @@ import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTable;
 
 public class BookView extends JFrame implements ActionListener {
-	private String may[][] = {{"일","월","화","수","목","금","토"},{" "," "," "," "," "," ","1일"},{"2일","3일","4일","5일","6일","7일","8일"},{"9일","10일","11일","12일","13일","14일","15일"},{"16일","17일","18일","19일","20일","21일","22일"},{"23일","24일","25일","26일","27일","28일","29일"},{"30일","31일"," ", " ", " ", " "," "}};
-	private String june[][] = {{"일","월","화","수","목","금","토"},{" "," ","1일","2일","3일","4일","5일"},{"6일","7일","8일","9일","10일","11일","12일"},{"13일","14일","15일","16일","17일","18일","19일"},{"20일","21일","22일","23일","24일","25일","26일"},{"27일","28일","29일","30일"," "," "," "},{" "," "," "," "," "," "," "}};
+	private String may[][] = {{" "," ", " ","5월", " "," "," "},{"일","월","화","수","목","금","토"},{" "," "," "," "," "," ","1일"},{"2일","3일","4일","5일","6일","7일","8일"},{"9일","10일","11일","12일","13일","14일","15일"},{"16일","17일","18일","19일","20일","21일","22일"},{"23일","24일","25일","26일","27일","28일","29일"},{"30일","31일"," ", " ", " ", " "," "}};
+	private String june[][] = {{" "," ", " ","6월", " "," "," "},{"일","월","화","수","목","금","토"},{" "," ","1일","2일","3일","4일","5일"},{"6일","7일","8일","9일","10일","11일","12일"},{"13일","14일","15일","16일","17일","18일","19일"},{"20일","21일","22일","23일","24일","25일","26일"},{"27일","28일","29일","30일"," "," "," "},{" "," "," "," "," "," "," "}};
 
 	//좌석
 	private String PSEATFF[][] = new String [5][5]; //1인 칸막이 - 여
 	private String PSEATFM[][] = new String [5][5]; //1인 칸막이 - 남
 	private String OSEAT[][] = new String [5][5]; // 오픈형
 	private String SROOM[] = new String [4]; // 스터디룸
-	
+	private String input;
 	//프로젝터
 	private int projector = 4;
 	
-	JPanel panel = new JPanel();
 	JTable table = new JTable();
-	JFrame booking = new JFrame();
-	
-	public void Booking5() {
-		//메뉴와 연결하고 이건 패널에 부착
-		this.setSize(360, 600);
-		this.setTitle("예약일 선택 (5월)");
-		this.setLayout(new GridLayout(7,7));
-		this.setBackground(new Color(249,242,242));
-
-		for(int i  = 0 ; i < may.length ; i++) {
-			for(int j = 0 ; j <may[i].length ; j ++) {
-				this.add(new JButton(may[i][j]));
-			}
-		}
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-	
-	public void Booking6() {
-		this.setSize(360,600);
-		this.setTitle("예약일 선택 (6월)");
-		this.setLayout(new GridLayout(7,7));
-		this.setBackground(new Color(249,242,242));
+	JButton button = new JButton();
+	JPanel panel = new JPanel();
 		
-		for(int i  = 0 ; i < june.length ; i++) {
-			for(int j = 0 ; j <june[i].length ; j ++) {
-				this.add(new JButton(june[i][j]));
+	public BookView() {}
+	
+	public void mon5() {
+		JFrame mon = new JFrame();
+		mon.setSize(360,600);
+		mon.setBackground(new Color(249,242,242));
+		mon.setTitle("예약일 선택");
+		mon.setLayout(new GridLayout(16, 16));
+		
+		for(int i = 0 ; i < may.length ; i ++) {
+			for(int j = 0 ; j < may[i].length ; j++) {
+				mon.add(new JButton(may[i][j])); 
 			}
 		}
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		for(int i = 0 ; i <	june.length ; i ++) {
+			for(int j = 0 ; j < june[i].length ; j++) {
+				mon.add(new JButton(june[i][j])); 
+			}
+		}
+	
+		
+		
+		mon.setVisible(true);
+		mon.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Date now = new Date();
+		
+		String[] selDate = { "5월 28일", "5월 29일", "5월 30일", "5월 31일", "6월 1일",
+				"6월 2일", "6월 3일", "6월 4일", "6월 5일", "6월 6일" ,"6월 7일", "6월 8일",
+				"6월 9일","6월 10일","6월 11일","6월 12일","6월 13일","6월 14일","6월 15일",
+				"6월 16일","6월 17일","6월 18일","6월 19일","6월 20일","6월 21일","6월 22일",
+				"6월 23일","6월 24일","6월 25일","6월 26일","6월 27일"
+		};
+		
+		 input = (String)JOptionPane.showInputDialog(
+				null,
+				
+				"오늘 " + now + " 기준으로 하단에 보이는 날짜만 선택이 가능합니다.",
+				"예약일 선택",
+				JOptionPane.INFORMATION_MESSAGE,
+				null,
+				selDate,
+				"5월 28일"
+				);
+		System.out.println(input);
+		mon.setVisible(false);
+		
+		JFrame seat = new JFrame();
+ 		seat.setVisible(true);
+ 		seat.setSize(360,600);
+		seat.setTitle("좌석을 선택하세요.");
+		seat.setBackground(new Color(249,242,242));
 
-	public void pkSeat() {
 		JButton block = new JButton("1인 칸막이 좌석");
-		JButton open = new JButton("1인 오픈형 좌석");
-		JButton company = new JButton("스터디룸");
-		JButton blank = new JButton(" ");
-						//리페인트..패널? 겟텏ㅌ스
-		this.setSize(360,600);
-		this.setTitle("좌석을 선택하세요.");
-		this.setBackground(new Color(249,242,242));
+ 		seat.add(block);
  		
- 		block.setLocation(40,170);
- 		block.setSize(270,50);
+		JButton open = new JButton("1인 오픈형 좌석");
+ 		seat.add(open);
 
+		JButton company = new JButton("스터디룸");
+ 		seat.add(company); 
+
+		seat.setLayout(null);
+		
+		block.setLocation(40,170);
+ 		block.setSize(270,50);
+ 		
  		open.setLocation(40, 240);
  		open.setSize(270,50);
-
+ 		
  		company.setLocation(40, 310);
  		company.setSize(270,50);
+ 		
+ 		block.addActionListener(new ActionListener() {
+ 			public void actionPerformed(ActionEvent e) {
+ 				JButton block = (JButton)e.getSource();
+ 				if(block.getText().equals("1인 칸막이 좌석")) {
+ 					seat.setVisible(false);
+ 					
+ 					JFrame ForWomen = new JFrame();
+ 					ForWomen.setSize(360,600);
+ 					ForWomen.setTitle("1인 칸막이 좌석 (여성전용)");
+ 					ForWomen.setLayout(new GridLayout(5,5));
+ 					ForWomen.setBackground(new Color(249,242,242));
+ 					
+ 					int num = 97;
+ 					
+ 					//초기화
+ 					for (int i = 0; i < PSEATFF.length; i++) {
+ 						for (int j = 0; j < PSEATFF[i].length; j++) {
+ 							if (j % 2 == 1) {
+ 								PSEATFF[i][j] = "거리두기";
+ 							} else {
+ 								PSEATFF[i][j] = "a , " + (char)(num++);
 
+ 							}
+ 						}
+ 					}
+ 					
+ 					//출력
+ 					for(int i = 0 ; i < PSEATFF.length ; i++) {
+ 						for( int j = 0 ; j <PSEATFF[i].length ; j ++) {
+ 							ForWomen.add(new JButton(PSEATFF[i][j]));
+ 						}
+ 					}
+ 					
+ 					ForWomen.setVisible(true);
+ 					ForWomen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 				}else if(open.getText().equals("1인 오픈형 좌석")) {
+ 					seat.setVisible(false);
+
+ 					JFrame open = new JFrame();
+ 					open.setSize(360,600);
+ 					open.setTitle("1인 오픈형 좌석");
+ 					open.setLayout(new GridLayout(5,5));
+ 					open.setBackground(new Color(249,242,242));
+ 					
+ 					int num = 97;
+ 					
+ 					//초기화
+ 					for (int i = 0; i < OSEAT.length; i++) {
+ 						for (int j = 0; j < OSEAT[i].length; j++) {
+ 							if (j % 2 == 1) {
+ 								OSEAT[i][j] = "거리두기";
+ 							} else {
+ 								OSEAT[i][j] = "c, " + (char)(num++);
+
+ 							}
+ 						}
+ 					}
+ 					
+ 					//출력
+ 					for(int i = 0 ; i < OSEAT.length ; i++) {
+ 						for( int j = 0 ; j <OSEAT[i].length ; j ++) {
+ 							open.add(new JButton(OSEAT[i][j]));
+ 						}
+ 					}
+ 					
+ 					open.setVisible(true);
+ 					open.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 					
+ 				}
+ 			}
+ 		});
  		
- 		blank.setLocation(40, 380);
- 		blank.setSize(270,50);
- 
- 		
- 		this.add(block);
- 		this.add(open);
- 		this.add(company); 
- 		this.add(blank);
- 		
- 		this.setVisible(true);
- 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+ 		seat.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
-	public void forWoman() {
-		this.setSize(360,600);
-		this.setTitle("1인 칸막이 좌석 (여성전용)");
-		this.setLayout(new GridLayout(5,5));
-		this.setBackground(new Color(249,242,242));
-		
-		int num = 97;
-		
-		//초기화
-		for (int i = 0; i < PSEATFF.length; i++) {
-			for (int j = 0; j < PSEATFF[i].length; j++) {
-				if (j % 2 == 1) {
-					PSEATFF[i][j] = "거리두기";
-				} else {
-					PSEATFF[i][j] = "a , " + (char)(num++);
-
-				}
-			}
-		}
-		
-		//출력
-		for(int i = 0 ; i < PSEATFF.length ; i++) {
-			for( int j = 0 ; j <PSEATFF[i].length ; j ++) {
-				this.add(new JButton(PSEATFF[i][j]));
-			}
-		}
-		
-		this.setVisible(true);
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-	}
 	
 	public void forMan() {
 		this.setSize(360,600);
@@ -269,7 +325,7 @@ public class BookView extends JFrame implements ActionListener {
 		this.setSize(360, 600);
 		this.setLocationRelativeTo(null);
 		this.setLayout(null);
-		panel.setBackground(new Color(249, 242, 242)); 
+		this.setBackground(new Color(249, 242, 242)); 
 		
 		JLabel image = new JLabel(new ImageIcon("images/humanIcon3.png"));
 		image.setBounds(34,80,54,54);
