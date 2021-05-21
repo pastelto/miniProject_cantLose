c7c4af3257bea5f203105248bac427cf78988466
Furaha-DH <pastelto@naver.com>
2021-05-21 14:47:23 +0900

    10. 메모 저장 및 메모 취소 작동 완료

--- src/com/kh/project/reservation/controller/BoardManager.java
+++ src/com/kh/project/reservation/controller/BoardManager.java
@@ -1,17 +1,22 @@
 package com.kh.project.reservation.controller;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class BoardManager { // 명언 & 보드
-	
+
 	public String maxim() {
-		
-		int num = (int)(Math.random()*10 + 1);
+
+		int num = (int) (Math.random() * 10 + 1);
 		String maxim = "";
-		switch(num) {
-		
-		case 1: 
+		switch (num) {
+
+		case 1:
 			maxim = "<html>	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;	창조의 모든 행위는 파괴에서 시작된다. <br><br>&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; -파블로 피카소</html>";
 			break;
-		case 2: 
+		case 2:
 			maxim = "<html>	 &nbsp; &nbsp; &nbsp; 	&nbsp; &nbsp; &nbsp; &nbsp; 	&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 절대 어제를 후회하지 마라. <br>&nbsp; 인생은 오늘의 나 안에 있고 내일은 스스로 만드는 것이다.<br> <br> &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;-L.론허바드</html>";
 			break;
 		case 3:
@@ -28,7 +33,7 @@ public class BoardManager { // 명언 & 보드
 			break;
 		case 7:
 			maxim = "<html>	 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp;   겨울이 오면 봄이 멀지 않으리 <br><br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;-셸리</html>";
-			break;	
+			break;
 		case 8:
 			maxim = "<html> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  자신감 있는 표정을 지으면 자신감이 생긴다 <br><br>&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;-찰스다윈</html>";
 			break;
@@ -37,16 +42,12 @@ public class BoardManager { // 명언 & 보드
 			break;
 		case 10:
 			maxim = "<html>	&nbsp;  오랫동안 꿈을 그리는 사람은 마침내 그 꿈을 닮아 간다 <br><br>&nbsp;  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;-앙드레 말로</html>";
-			
-			break;	
+
+			break;
 		}
 		return maxim;
-		
+
 	}
-	
-	public void myMemo() { // 메모장
-		
-		
-	}
+
 
 }
